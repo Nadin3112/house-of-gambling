0db5c863115e487fbf4b6947a94d73dc86e7a7d2
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2024-01-19 15:22:58 +0300

    adds theme

---
+++ src/styles/Theme.tsx
@@ -0,0 +1,20 @@
+export const theme = {
+  colors: {
+    primaryBg: "#110a1d",
+    font:"#ffffff",
+    accent: "#478bf9",
+    borderBtn: "#9dadf2",
+    bgCFooter: "#1e1231",
+    bgBlog: "rgba(255, 255, 255, 0.12)"
+  },
+
+  media: {
+    tablet: "screen and (max-width: 768px)",
+    mobile: "screen and (max-width: 576px)",
+    desktop: "screen and (min-width: 1140px)"
+  },
+
+  animations: {
+    transition: " .2s ease-in-out"
+  }
+}
