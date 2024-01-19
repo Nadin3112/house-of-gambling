ea6a33baaad58c6281a181d2ba6950e461f6fe9e
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2024-01-19 15:25:29 +0300

    adds components

---
+++ src/components/Button.ts
@@ -0,0 +1,33 @@
+import styled, {css} from 'styled-components';
+import { theme } from '../styles/Theme';
+
+type StyledBtnPropsType = {
+    backgroundColor?: string
+    btnType: "primary" | "outlined"
+}
+
+export const Button = styled.button<StyledBtnPropsType>`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 14px;
+    font-family: 'Lexend', sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 20px;
+    border-radius: 24px;
+    cursor: pointer;
+    color: ${theme.colors.font};
+    text-transform: uppercase;
+
+
+    ${props => props.btnType === "primary" && css<StyledBtnPropsType>`
+        background-color: ${theme.colors.accent};
+        border: none;
+    `}
+        
+    ${props => props.btnType === "outlined" && css<StyledBtnPropsType>`
+        background-color: transparent;
+        border: 2px solid ${theme.colors.accent};
+    `}
+`
---
+++ src/components/Container.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1640px;
+  width: 100%;
+  min-width: 300px;
+  min-height: 100%;
+  padding: 0 10px;
+  margin: 0 auto;
+`
