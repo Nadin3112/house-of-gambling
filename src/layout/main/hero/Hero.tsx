f862ded7f9ce19493a5b5bb879ed1fb7489a4e5f
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2024-01-25 16:02:47 +0300

    refractoring hero

--- src/layout/main/hero/Hero.tsx
+++ src/layout/main/hero/Hero.tsx
@@ -1,89 +1,14 @@
 import React from 'react';
 import { Button } from '../../../components/Button';
-import styled from 'styled-components';
-import { theme } from '../../../styles/Theme';
+import { S } from './Hero_Styles';
 
 export const Hero:React.FC = () => {
     return (
-        <HeroStyled>
-            <HeroTitle>House of <span>gambling</span></HeroTitle>
-            <Text>Raise your ROI with direct advertiser</Text>
+        <S.Hero>
+            <S.HeroTitle>House of <span>gambling</span></S.HeroTitle>
+            <S.HeroText>Raise your ROI with direct advertiser</S.HeroText>
             <Button btnType={'primary'}>Become a Partner</Button>
-        </HeroStyled>
+        </S.Hero>
     );
 };
 
-const HeroStyled = styled.section`
-    max-width: 658px;
-    padding: 296px 0 0 8px;
-    
-    @media ${theme.media.desktop} {
-        padding: 70px 0 0;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    }
-    
-    ${Button} {
-        max-width: 394px;
-        min-width: 300px;
-        width: 100%;
-        padding: 20px 72px;
-        border-radius: 48px;
-        font-weight: 600;
-        font-size: 24px;
-        line-height: 1.3;
-        text-transform: uppercase;
-
-        @media ${theme.media.desktop} {
-            align-self: center;
-        }
-
-        @media ${theme.media.tablet} {
-            padding: 15px 20px;
-            font-size: 18px; 
-        }
-    }
-`
-
-const HeroTitle = styled.h1`
-    margin: 0;
-    margin-bottom: 24px;
-    font-weight: 500;
-    font-size: 96px;
-    line-height: 1;
-    text-transform: uppercase;
-
-    @media ${theme.media.desktop} {
-        text-align: center;
-    }
-
-    span {
-        color: ${theme.colors.accent};
-    }
-
-    @media ${theme.media.tablet} {
-        line-height: 50px;
-        margin-bottom: 12px;
-        font-size: 46px;
-    }
-`
-
-const Text = styled.p`
-    color: ${theme.colors.fontText};
-    font-weight: 500;
-    font-size: 32px; 
-    line-height: 1;
-    margin-bottom: 48px;
-
-    @media ${theme.media.desktop} {
-        text-align: center;
-    }
-
-    @media ${theme.media.mobile} {
-        line-height: 26px;
-        margin-bottom: 24px;
-        font-size: 20px;
-
-    }
-`
---
+++ src/layout/main/hero/Hero_Styles.ts
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+import { theme } from '../../../styles/Theme';
+import { Button } from '../../../components/Button';
+
+
+const Hero = styled.section`
+    max-width: 658px;
+    padding: 296px 0 0 8px;
+    
+    @media ${theme.media.desktop} {
+        padding: 70px 0 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    
+    ${Button} {
+        max-width: 394px;
+        min-width: 300px;
+        width: 100%;
+        padding: 20px 72px;
+        border-radius: 48px;
+        font-weight: 600;
+        font-size: 24px;
+        line-height: 1.3;
+        text-transform: uppercase;
+
+        @media ${theme.media.desktop} {
+            align-self: center;
+        }
+
+        @media ${theme.media.tablet} {
+            padding: 15px 20px;
+            font-size: 18px; 
+        }
+    }
+`
+
+const HeroTitle = styled.h1`
+    margin: 0;
+    margin-bottom: 24px;
+    font-weight: 500;
+    font-size: 96px;
+    line-height: 1;
+    text-transform: uppercase;
+
+    @media ${theme.media.desktop} {
+        text-align: center;
+    }
+
+    span {
+        color: ${theme.colors.accent};
+    }
+
+    @media ${theme.media.tablet} {
+        line-height: 50px;
+        margin-bottom: 12px;
+        font-size: 46px;
+    }
+`
+
+const HeroText = styled.p`
+    color: ${theme.colors.fontText};
+    font-weight: 500;
+    font-size: 32px; 
+    line-height: 1;
+    margin-bottom: 48px;
+
+    @media ${theme.media.desktop} {
+        text-align: center;
+    }
+
+    @media ${theme.media.mobile} {
+        line-height: 26px;
+        margin-bottom: 24px;
+        font-size: 20px;
+    }
+`
+
+export const S = {
+    Hero, 
+    HeroTitle,
+    HeroText
+}
